011118c8a6b95e247ae8a005187cf9288253671d
Michael Propp <michaelpropp8664@gmail.com>
2024-02-19 22:05:49 -0500

    requirements.txt

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.event_handler.api_gateway import Router
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
 import json
 
 import client
@@ -33,7 +35,7 @@ def get_incomes():
 
 app.include_router(router)
 
-def lambda_handler(event: dict, context):
+def lambda_handler(event: dict, context: LambdaContext):
     print('Received event: ', event)
     print('Received context: ', context)
     return app.resolve(event, context)
