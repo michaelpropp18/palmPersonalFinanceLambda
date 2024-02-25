9e54b0f18791dcfc95486b1f1e07ffbf1b4cbeba
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 16:20:30 -0500

    fix

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import json
 import boto3
 from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
+
 from typing import List
 
-from models.expense import Expense, ExpenseDynamoDb
+from models.expense import Expense
 
 dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
 
@@ -14,9 +17,10 @@ def get_expenses() -> List[Expense]:
     return res['Items']
 
 
-def put_expense(expense: ExpenseDynamoDb):
+def put_expense(expense: Expense):
     table = dynamodb.Table(EXPENSES_TABLE_NAME)
     expense_dict = expense.dict(by_alias=True)
     print(expense_dict)
-    res = table.put_item(Item=expense_dict)
+    print(json.dumps(expense_dict))
+    res = table.put_item(Item=json.dumps(expense_dict))
     return res
