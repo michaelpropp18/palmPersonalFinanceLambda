e3b2825e24b87a86edd6d5809683455fb82c093f
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 11:27:43 -0500

    test

--- expenses.py
+++ expenses.py
@@ -25,15 +25,12 @@ def get_expenses():
     }
 
 @router.post("/expenses")
-def put_expense(expense: Expense) -> int:
+def put_expense(expense: Expense) -> str:
     expense_data: dict = router.current_event.body  # deserialize json str to dict
     #body = json.loads(router.current_event['body'])
     print(expense_data)
     print(expense.dict(by_alias=True))
-    return {
-        'statusCode': 200,
-        'body': 'yay'
-    }
+    return "1"
     '''
     if (expense_data and 'amount' in expense_data and 'id' in expense_data and 'name' in expense_data):
         res = db_client.put_item(
