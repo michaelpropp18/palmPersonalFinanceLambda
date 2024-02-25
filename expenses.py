f77c7b0e3e2f3ccd75c93d04bae2f92a15fe6034
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 17:42:18 -0500

    clean up imports

--- expenses.py
+++ expenses.py
@@ -1,20 +1,10 @@
 import json
 from aws_lambda_powertools.event_handler.api_gateway import Router
-from pydantic import BaseModel, Field
-from datetime import datetime, timezone
-from uuid import uuid4
-from typing import List, Optional
+from typing import List
 
-import client
 import database
 
-import boto3
-from boto3.dynamodb.conditions import Key
-
 from models.expense import *
-
-router = Router()
-db_client = client.initiate_client()
 
 @router.get("/expenses")
 def get_expenses() -> List[Expense]:
