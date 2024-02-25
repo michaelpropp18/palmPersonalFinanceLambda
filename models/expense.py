909fd237d45e711710b9f4e6e9a5f87bdcc2f84d
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 15:33:56 -0500

    try decimal

--- models/expense.py
+++ models/expense.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 from uuid import uuid4
+from decimal import Decimal
 
 class Expense(BaseModel): 
-    amount: float 
+    amount: Decimal 
     id_: int = Field(alias="id", default_factory=lambda: uuid4().int) 
     name: str
     datetime_: datetime = Field(alias="datetime", default_factory=lambda: datetime.now(timezone.utc))
