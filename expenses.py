ad5a3090c864c65c2fabdcf049bc068df9886b8a
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 13:55:34 -0500

    test

--- expenses.py
+++ expenses.py
@@ -25,9 +25,12 @@ def get_expenses():
     table = dynamodb.Table("Expenses")
     res = table.scan()
     print(res)
+    print(res.items)
+    item = res.items[0]
+    print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps(res)
+        'body': 'yay'
     }
     '''
     res = db_client.scan(TableName="Expenses")["Items"]
