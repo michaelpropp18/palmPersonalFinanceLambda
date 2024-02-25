642b5321cb704ee14c399858750ecff2be92b392
Michael Propp <michaelpropp8664@gmail.com>
2024-02-25 13:57:14 -0500

    fix

--- expenses.py
+++ expenses.py
@@ -25,8 +25,8 @@ def get_expenses():
     table = dynamodb.Table("Expenses")
     res = table.scan()
     print(res)
-    print(res.items)
-    item = res.items[0]
+    print(res['items'])
+    item = res['items'][0]
     print(item)
     return {
         'statusCode': 200,
