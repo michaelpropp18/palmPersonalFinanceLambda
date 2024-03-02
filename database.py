9c2d601d9de968eeb04432b38d9c880d5964720c
Michael Propp <michaelpropp8664@gmail.com>
2024-03-01 20:59:28 -0500

    add incomes endpoints

--- database.py
+++ database.py
@@ -17,6 +17,10 @@ REGION_NAME = "us-east-1"
 
 dynamodb = boto3.resource('dynamodb', region_name=REGION_NAME)
 
+##############################################################
+# Expenses
+##############################################################
+
 def get_expenses() -> List[Expense]:
     table = dynamodb.Table(EXPENSES_TABLE_NAME)
     res = table.scan()
@@ -60,11 +64,56 @@ def delete_expense(expense_id: str) -> Expense:
     if res['ResponseMetadata']['HTTPStatusCode'] == 200 and 'Attributes' in res:
         return res['Attributes']
 
+##############################################################
+# Incomes
+##############################################################
 
 def get_incomes() -> List[Income]:
     table = dynamodb.Table(INCOMES_TABLE_NAME)
     res = table.scan()
     return res['Items']
+
+def get_income(income_id: str) -> Income:
+    table = dynamodb.Table(INCOMES_TABLE_NAME)
+    res = table.get_item(Key={'id': income_id})
+    if 'Item' in res:
+        return res['Item']
+
+def post_income(income: Income) -> Income:
+    table = dynamodb.Table(INCOMES_TABLE_NAME)
+    income_dict = income.dict(by_alias=True)
+    res = table.put_item(Item=_prepare_dynamodb_dict(income_dict))
+    print(res)
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return expense
+
+def put_income(income: Income) -> Income:
+    table = dynamodb.Table(INCOMES_TABLE_NAME)
+    income_dict = income.dict(by_alias=True)
+    try:
+        res = table.put_item(
+            Item=_prepare_dynamodb_dict(income_dict),
+            ConditionExpression=Attr('id').eq(income.id_),
+            ReturnValues='ALL_OLD'
+        )
+    except ClientError as e:
+        print(e)
+        return
+    print(res)
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200 and 'Attributes' in res:
+        return res['Attributes']
+    
+
+def delete_income(income_id: str) -> Income:
+    table = dynamodb.Table(INCOMES_TABLE_NAME)
+    res = table.delete_item(Key={'id': income_id}, ReturnValues="ALL_OLD")
+    print(res)
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200 and 'Attributes' in res:
+        return res['Attributes']
+
+##############################################################
+# Utilities
+##############################################################
 
 # remove values that DynamoDB doesn't like
 def _prepare_dynamodb_dict(d):
--- incomes.py
+++ incomes.py
@@ -10,3 +10,19 @@ router = Router()
 @router.get("/incomes")
 def get_incomes() -> List[Income]:
     return database.get_incomes()
+
+@router.get("/incomes/<income_id>")
+def get_income(income_id: str) -> Income:
+    return database.get_income(income_id)
+
+@router.post("/incomes")
+def post_income(income: Income) -> Income:
+    return database.post_income(income)
+
+@router.put("/incomes")
+def put_income(income: Income) -> Income:
+    return database.put_income(income)
+
+@router.delete("/incomes/<income_id>")
+def delete_incomes(income_id: str) -> Income:
+    return database.delete_income(income_id)
