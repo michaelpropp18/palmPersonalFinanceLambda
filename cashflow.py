844a46322f896d837da36372c14cfd7975d42ca5
Michael Propp <michaelpropp8664@gmail.com>
2024-03-10 17:36:58 -0400

    cashflow summary

--- cashflow.py
+++ cashflow.py
@@ -1,6 +1,4 @@
 from aws_lambda_powertools.event_handler.api_gateway import Router
-from aws_lambda_powertools.shared.types import Annotated  
-from aws_lambda_powertools.event_handler.openapi.params import Query  
 
 from typing import List, Optional
 from datetime import datetime
@@ -13,15 +11,19 @@ from models.income import Income
 router = Router()
 
 @router.get("/cashflow")
-def get_cashflow(
+def get_cashflow_summary(
     start: Optional[datetime] = None, 
     end: Optional[datetime] = None
 ) -> float:
-    expenses: List[Expense] = database.get_expenses(start, end)
     incomes: List[Income] = database.get_incomes(start, end)
-    cashflow = 0
-    for e in expenses:
-        cashflow -= e['amount']
-    for i in incomes:
-        cashflow += i['amount']
-    return cashflow
+    expenses: List[Expense] = database.get_expenses(start, end)
+
+    income = sum([i['amount'] for i in incomes])
+    expenses = sum([i['amount'] for i in expenses])
+    cashflow = income - expenses
+
+    return {
+        'income': income,
+        'expenses': expenses,
+        'cashflow': cashflow
+    }
